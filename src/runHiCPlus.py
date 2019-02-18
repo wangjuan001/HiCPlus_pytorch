252cc8c61d8caa2aaaf842a426c677a40d604ffa
wangjuan001 <275652269@qq.com>
2019-02-17 21:07:25 -0500

    Created using Colaboratory

--- src/runHiCPlus.py
+++ src/runHiCPlus.py
@@ -1,9 +1,10 @@
 # Author: Yan Zhang  
 # Email: zhangyan.cse (@) gmail.com
 
+import argparse
 import sys
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import pickle
 import os
 import gzip
@@ -17,8 +18,27 @@ import sys
 import torch.nn as nn
 import utils
 import math
+from datetime import datetime
 
-use_gpu = 1
+
+startTime = datetime.now()
+
+parser = argparse.ArgumentParser(description='PyTorch Super Res Example')
+parser.add_argument('--input_matrix', type=str, required=True, help='input matrix to use')
+parser.add_argument('--model', type=str, required=True, help='model file to use')
+parser.add_argument('--output_filename', type=str, help='where to save the output image')
+parser.add_argument('--scale_factor', type=float, help='factor by which super resolution needed')
+parser.add_argument('--chr', type=int,required=True, help='chromosome number')
+parser.add_argument('--cuda', action='store_true', help='use cuda')
+opt = parser.parse_args()
+
+print(opt)
+
+use_gpu = opt.cuda
+if use_gpu and not torch.cuda.is_available():
+    raise Exception("No GPU found, please run without --cuda")
+
+#use_gpu = 1
 
 conv2d1_filters_numbers = 8
 conv2d1_filters_size = 9
@@ -33,10 +53,20 @@ epochs = 10
 HiC_max_value = 100
 
 
+chrs_length = [249250621,243199373,198022430,191154276,180915260,171115067,159138663,146364022,141213431,135534747,135006516,133851895,115169878,107349540,102531392,90354753,81195210,78077248,59128983,63025520,48129895,51304566]
 
-# This block is the actual training data used in the training. The training data is too large to put on Github, so only toy data is used. 
-input_file = '/home/zhangyan/Desktop/chr21.10kb.matrix'
-low_resolution_samples, index = utils.divide(input_file)
+chrN = opt.chr
+
+expRes = 10000
+## need to make resolution adjustable.
+length = chrs_length[chrN-1]/expRes
+
+# divide the input matrix into sub-matrixes. 
+input_file = opt.input_matrix
+
+inputMatrix = utils.readFiles(input_file, length + 1, expRes)
+
+low_resolution_samples, index = utils.divide(inputMatrix)
 
 low_resolution_samples = np.minimum(HiC_max_value, low_resolution_samples)
 
@@ -49,7 +79,7 @@ half_padding = padding / 2
 output_length = sample_size - padding
 
 
-print low_resolution_samples.shape
+print(low_resolution_samples.shape)
 
 lowres_set = data.TensorDataset(torch.from_numpy(low_resolution_samples), torch.from_numpy(np.zeros(low_resolution_samples.shape[0])))
 lowres_loader = torch.utils.data.DataLoader(lowres_set, batch_size=batch_size, shuffle=False)
@@ -57,16 +87,16 @@ lowres_loader = torch.utils.data.DataLoader(lowres_set, batch_size=batch_size, s
 hires_loader = lowres_loader
 
 model = model.Net(40, 28)
-model.load_state_dict(torch.load('../model/pytorch_model_12000'))
+model.load_state_dict(torch.load(opt.model))
 if use_gpu:
     model = model.cuda()
 
-_loss = nn.MSELoss()
+#_loss = nn.MSELoss()
 
 
-running_loss = 0.0
-running_loss_validate = 0.0
-reg_loss = 0.0
+#running_loss = 0.0
+#running_loss_validate = 0.0
+#reg_loss = 0.0
 
 
 for i, (v1, v2) in enumerate(zip(lowres_loader, hires_loader)):    
@@ -83,13 +113,13 @@ for i, (v1, v2) in enumerate(zip(lowres_loader, hires_loader)):
     y_prediction = model(_lowRes)
 
     
-print '-------', i, running_loss, strftime("%Y-%m-%d %H:%M:%S", gmtime())
+#print '-------', i, running_loss, strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
 
 y_predict = y_prediction.data.cpu().numpy()
 
 
-print y_predict.shape
+print(y_predict.shape)
 
 # recombine samples
 
@@ -97,28 +127,27 @@ length = int(y_predict.shape[2])
 y_predict = np.reshape(y_predict, (y_predict.shape[0], length, length))
 
 
-chrs_length = [249250621,243199373,198022430,191154276,180915260,171115067,159138663,146364022,141213431,135534747,135006516,133851895,115169878,107349540,102531392,90354753,81195210,78077248,59128983,63025520,48129895,51304566]
-
-chrN = 21
-
-length = chrs_length[chrN-1]/10000
+length = int(chrs_length[chrN-1]/expRes)
 
 prediction_1 = np.zeros((length, length))
 
 
-print 'predicted sample: ', y_predict.shape, '; index shape is: ', index.shape
+print('predicted sample: ', y_predict.shape, '; index shape is: ', index.shape)
 #print index
 for i in range(0, y_predict.shape[0]):          
     if (int(index[i][1]) != chrN):
         continue
-#print index[i]
-x = int(index[i][2])
-y = int(index[i][3])
-#print np.count_nonzero(y_predict[i])
-prediction_1[x+6:x+34, y+6:y+34] = y_predict[i]
+    #print index[i]
+    x = int(index[i][2])
+    y = int(index[i][3])
+    #print np.count_nonzero(y_predict[i])
+    prediction_1[x+6:x+34, y+6:y+34] = y_predict[i]
 
 np.save(input_file + 'enhanced.npy', prediction_1)
 
+print(datetime.now() - startTime) 
+
+np.savetxt(input_file + 'enhanced.txt',prediction_1, fmt='%d', delimiter="\t")
 
 
 
